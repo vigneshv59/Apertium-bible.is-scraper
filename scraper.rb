1dd33bf83695d0921533cd6214b79b54ab02a752
Vignesh Varadarajan <vigneshv@gmail.com>
2015-12-12 14:04:13 -0800

    Added time as arg.

--- scraper.rb
+++ scraper.rb
@@ -13,6 +13,10 @@ optparse = OptionParser.new do |opts|
   opts.on("-oOUTPUT", "--out=OUTPUT", "The out file name. (Mandatory)") do |o|
     options[:out] = o
   end
+  
+  opts.on("-tTIME", "--time=TIME", "The time limit. (Optional)") do |t|
+    options[:time] = t
+  end
 end
 
 begin
@@ -26,8 +30,10 @@ end
 bible_url = options[:url]  || "http://www.bible.is/ENGESV/2Pet/3"
 page_exists = true
 
-open(options[:out] || "out.txt", "w:UTF-16") do |f|
-  while page_exists
+start_time = Time.now
+
+open(options[:out] || "out.txt", "w:UTF-8") do |f|
+  while page_exists && (options[:time].nil? || Time.now - start_time < options[:time].to_i)
     page = Nokogiri::HTML(open(bible_url).read, nil, 'UTF-8')
     a = page.css(".verse-container")
   
